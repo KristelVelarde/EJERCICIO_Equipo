ad2a6c9d7197c01318d668326d669201a27c16e9
KristelVelarde <A01741033@itesm.mx>
2021-10-11 22:44:17 +0000

    Proyecto_integrador

--- assignments/00HelloWorld/src/exercise.py
+++ assignments/00HelloWorld/src/exercise.py
@@ -1,6 +1,21 @@
+def imprime_grafica1_Anya():
+    pass
+
+def imprime_grafica2_Ale():
+    pass
+
+def imprime_grafica3_Kristel():
+    pass
+
 def main():
     #escribe tu código abajo de esta línea
     pass
+imprime_grafica1_Anya()
+imprime_grafica2_Ale()
+imprime_grafica3_Kristel()
+
+
 
 if __name__=='__main__':
     main()
+

93734b4c051d954798b89a96da8decb8d9d37853
KristelVelarde <A01741033@itesm.mx>
2021-10-13 20:00:12 +0000

    Ejericio_equipo

--- assignments/00HelloWorld/src/exercise.py
+++ assignments/00HelloWorld/src/exercise.py
@@ -1,15 +1,29 @@
 def imprime_grafica1_Anya():
-    pass
+    
+    pass 
 
 def imprime_grafica2_Ale():
     pass
 
 def imprime_grafica3_Kristel():
+   
     pass
 
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    print("LAS ADDICIONES A LAS REDES SOCIALES")
+    p1=input("Cual es tu nombre: ")
+    p2=input("Cuantos años tienes: ")
+    p3=input("Crees que eres addicto a las redes sociales?: ")
+    if len(p2)<=13:
+        print("Eres un pequeñx en proceso de aprender y equivocarte")
+    elif len(p2)>=25:
+        print("Ya estas grandecitx y sabes lo que haces")
+    else:
+        print("Eres un adolescente en proceso de descubrirte a ti mismo")
+
+
+
 imprime_grafica1_Anya()
 imprime_grafica2_Ale()
 imprime_grafica3_Kristel()
